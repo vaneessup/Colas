735b73964a63f06b98a8c051f2234e4a73fc289e
vaneessup <vanessaramirez.vr45@gmail.com>
2021-02-19 18:27:15 -0600

    version 1

---
+++ clases/Pacientes.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colas.clases
+{
+    //crear clase paciente
+    class Pacientes
+    {
+        string codigo;
+        public string Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        string nombrep;
+
+        public string Nombre
+        {
+            get { return nombrep; }
+            set { nombrep = value; }
+        }
+
+        string apellido;
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        string estado;
+
+        public string Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+
+
+
+        //metodos
+
+        public void ingresarDatos()
+        {
+            Console.WriteLine("\nIngrese el codigo del paciente: ");
+            codigo = Console.ReadLine();
+            Console.WriteLine("\nIngrese el nombre del paciente: ");
+            nombrep = Console.ReadLine();
+            Console.WriteLine("\nIngrese el apellido del paciente: ");
+            apellido = Console.ReadLine();
+            Console.WriteLine("\nIngrese el estado del paciente: ");
+            estado = Console.ReadLine();
+        }
+
+        struct cola
+        {
+            string codigo1;
+            string nombre1;
+            string apellido1;
+            string estado;
+
+        };
+
+        cola colaCreate()
+        {
+            cola cola1;
+            cola1.ToString().codigo1 = -1;
+            return cola1;
+
+        }
+
+        
+    }
+
+    
+}
---
+++ clases/clsColas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colas.clases
+{
+    class clsColas
+    {
+        public void cola()
+        {
+            Pacientes[] pacientes = new Pacientes[10];
+
+           
+        }
+
+
+    }
+}
